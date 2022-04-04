5dc50586856170de5a7fadd884e84b818ddc9cc2
amig01 <59666322+amig01@users.noreply.github.com>
2022-04-05 03:31:39 +0500

    Project2.cpp
    Added task list

--- Project2.cpp
+++ Project2.cpp
@@ -1,76 +1,76 @@
 #include <iostream>
+#include <string>
 using namespace std;
-
-class Bus
-{
-protected:
-	string strBusID;	 // String Bus ID
-	static int bcounter; // Variable to Initialize IDs
-
-public:
-	Bus()
-	{
-		strBusID = 'B' + to_string(++bcounter);
-	}
-
-	string getBusID() { return strBusID; }
-};
-
-class Passenger
-{
-protected:
-	string strPassengerID; // String Passenger ID
-	static int pcounter;   // Variable to Initialize IDs
-
-public:
-	Passenger()
-	{
-		strPassengerID = 'P' + to_string(++pcounter);
-	}
-
-	string getPassengerID() { return strPassengerID; }
-};
 
 class Seat
 {
 protected:
-	string seatID[10][6];
-
+	string m_seatID[20]; // String Seats ID
+	static int scounter; // Variable to Initialize Seats IDs
 public:
 	Seat()
 	{
-		for (int r = 1; r < 10; r++)
-		{
-			for (int c = 1; c <= 5; c++)
-			{
-				seatID[r][c] = 'S' + to_string(r) + to_string(c);
-			}
-		}
+		for (int i = 0; i < 20; i++)
+			m_seatID[i] = 'S' + (scounter < 9 ? to_string(0) : "") + to_string(++scounter);
 	}
+	const string& operator [](int index) const { return m_seatID[index]; }
 };
+int Seat::scounter;
 
-int Passenger::pcounter = 0;
-int Bus::bcounter = 0;
+class Bus
+{
+protected:
+	string m_busID;	 // String Bus ID
+	Seat m_bus_seats; //Array of seats in bus
+	static int bcounter; // Variable to Initialize Bus IDs
+public:
+	Bus() : m_busID('B' + (bcounter < 9 ? to_string(0) : "") + to_string(++bcounter)) {}
+	string getBusID() const { return m_busID; }
+	const string& getSeatID(int index) const { return m_bus_seats[index]; } 
+};
+int Bus::bcounter;
+
+class Passenger
+{
+protected:
+	string m_passengerID; // String Passenger ID
+	static int pcounter;   // Variable to Initialize Passenger IDs
+	string m_passenger_seatID; //String Passenger Seat ID
+public:
+	Passenger() : m_passengerID('P' + (pcounter < 9 ? to_string(0) : "") + to_string(++pcounter)) {}
+	const string& getPassengerID() const { return m_passengerID; }
+	void setPassengerSeatID(const string& ID) { m_passenger_seatID = ID; }
+	const string& getPassengerSeatID() const { return m_passenger_seatID; }
+};
+int Passenger::pcounter;
 
 int main()
 {
-	/*Bus buses[10];
-
-	for (int i = 0; i < 10; i++)
+	Bus* buses = new Bus[1];
+	for (int i = 0; i < 1; i++)
 	{
-		cout << buses[i].getBusID() << " ";
+		cout << "\tBus " << buses[i].getBusID() << endl;
+		for (int j = 0; j < 20; j++)
+		{
+			cout << "Seat " << buses[i].getSeatID(j);
+			j % 2 == 0 ? cout << '\t' : cout << '\n';
+		}
 	}
-
 	cout << endl;
 
-	Passenger passenger[10];
+	Passenger* passengers = new Passenger[1];
+	for (int i = 0; i < 1; i++)
+		cout << "Passenger " << passengers[i].getPassengerID() << endl;
 
-	for (int i = 0; i < 10; i++)
-	{
-		cout << passenger[i].getPassengerID() << " ";
-	}*/
+	passengers[0].setPassengerSeatID(buses[0].getBusID() + buses[0].getSeatID(2));
 
-	Seat seats;
+	cout << "\n\nPassenger information:" << endl;
+	cout << "Passenger's ID: " << passengers[0].getPassengerID() << endl;
+	cout << "Passenger's seat ID: " << passengers[0].getPassengerSeatID() << endl;
 
 	return 0;
 }
+
+//Add try catch
+//Add menu
+//Add header
